8815386dea11651b14d029a665dd6893d594e1f8
riamcodes <138901333+riamcodes@users.noreply.github.com>
2023-07-07 17:41:55 -0500

    Delete README.d

--- README.d
+++
@@ -1,4 +0,0 @@
-#  Cloned from Ria's SMU git
-
----
-<a href="https://www.buymeacoffee.com/cturra" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-yellow.png" alt="Buy Me A Coffee" height="41" width="174"></a>

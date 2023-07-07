0d85432858b0ad83c615b2ac24028d42c3689fbf
riamcodes <138901333+riamcodes@users.noreply.github.com>
2023-07-07 17:35:56 -0500

    Create README.d

---
+++ README.d
@@ -0,0 +1,4 @@
+# Cloned from Ria's SMU git
+
+     ---
+<a href="https://www.buymeacoffee.com/cturra" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-yellow.png" alt="Buy Me A Coffee" height="41" width="174"></a>
